1b680f88d58e13733727060e7de42f383f4b1495
Kv.se7en <shaoxinheng@gmail.com>
2019-10-16 22:27:42 +0800

    up

--- server/main.go
+++ server/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-   "server/route"
+	"server/route"
 )
 
 var a string
 
 func main() {
-   a = "G"
-   print(route.RouterName)
-   f1()
+	a = "G"
+	print(route.RouterName)
+	f1()
 }
 
 func f1() {
-   a := "O"
-   print(a)
-   f2()
+	a := "O"
+	print(a)
+	f2()
 }
 
 func f2() {
-   print(a)
+	print(a)
 }
