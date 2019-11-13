0da8c9196f98a2c324326f017bd2869eccc8a9d4
xinheng.shao <shaoxinheng@gmail.com>
2019-11-13 19:07:09 +0800

    up

--- app/utils/http.js
+++ app/utils/http.js
@@ -1,17 +1,21 @@
 import Tools from './tools';
 
-const headers = {
-  "Connection": ""
-}
+const REQUEST_URL = "http://172.16.46.204:8080/v1"
+
 async function get(url, params) {
   try {
+
+    // 简化的 url
+    if (url.indexOf('http') < 0) {
+      url = REQUEST_URL + url;
+    }
     if (params) {
       let queryString = Tools.convertParamsToGetString(params);
       url = url + (queryString ? "?" + queryString : "");
     }
+
     const res = await fetch(url, {
-      method: 'GET',
-      headers: headers,
+      method: 'GET'
     });
     return res.json();
   } catch (err) {
