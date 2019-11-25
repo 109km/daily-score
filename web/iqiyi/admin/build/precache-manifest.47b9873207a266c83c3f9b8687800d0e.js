9b58a40552288319b43601fced50c5eb005c0404
xinheng.shao <shaoxinheng@gmail.com>
2019-11-25 15:30:15 +0800

    up

---
+++ web/iqiyi/admin/build/precache-manifest.47b9873207a266c83c3f9b8687800d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170852cff4ed3915457f81dee31c70c2",
+    "url": "https://ad-1251636704.file.myqcloud.com/admin-iqiyi-1029/index.html"
+  },
+  {
+    "revision": "a86fcb17486650a91198",
+    "url": "https://ad-1251636704.file.myqcloud.com/admin-iqiyi-1029/static/css/2.1dcc7ebc.chunk.css"
+  },
+  {
+    "revision": "fe3f09030a9abb5c7f10",
+    "url": "https://ad-1251636704.file.myqcloud.com/admin-iqiyi-1029/static/css/main.ac94039b.chunk.css"
+  },
+  {
+    "revision": "a86fcb17486650a91198",
+    "url": "https://ad-1251636704.file.myqcloud.com/admin-iqiyi-1029/static/js/2.7c1b8df6.chunk.js"
+  },
+  {
+    "revision": "fe3f09030a9abb5c7f10",
+    "url": "https://ad-1251636704.file.myqcloud.com/admin-iqiyi-1029/static/js/main.5a6ea371.chunk.js"
+  },
+  {
+    "revision": "521262a6f3a8b271938b",
+    "url": "https://ad-1251636704.file.myqcloud.com/admin-iqiyi-1029/static/js/runtime-main.430def0e.js"
+  }
+]);
